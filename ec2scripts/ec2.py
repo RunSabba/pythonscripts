85ef90032ec54114bfcb0b02588655c3611d7a3b
RunSabba <abbasnur24@gmail.com>
2024-09-24 08:59:40 -0400

    added additional tag

--- ec2scripts/ec2.py
+++ ec2scripts/ec2.py
@@ -3,19 +3,40 @@ from dotenv import load_dotenv
 import boto3
 load_dotenv()
 
-#crerating an ec2 using a ec2 resource
-ec2_resource = boto3.resource("ec2")
-my_instance = "RunSabba Cloud"
+#creating an ec2 using a ec2 resource
+region_name = "us-east-1"
+ami = "ami-0e86e20dae9224db8"
+instance_owner = "Runsabba-CloudOps"
+ec2_resource = boto3.resource("ec2", region_name = region_name)
+
 
 #variable for my key_pair
 EC2_key = os.getenv('EC2_KEY')
 #variable for my bashscript installing nginx and my webserver, *add 3 quotes for script*
-nginx_script = 
+nginx_script = '''#!/bin/bash
+# Log start of script execution
+echo "Starting user data script execution..." > /var/log/user-data.log
+
+# Update packages and install nginx
+sudo apt update
+sudo apt upgrade -y
+sudo apt install nginx -y
+
+# Enable and start Nginx
+sudo systemctl enable nginx
+sudo systemctl start nginx
+
+# Create the custom index.html
+echo "<html><body><h1>Hello from RunSabba Web Server 1!</h1></body></html>" | sudo tee /var/www/html/index.html
+
+# Log the end of script execution
+echo "User data script execution has completed." >> /var/log/user-data.log
+'''
 
 
 
 nginx_instance = ec2_resource.create_instances(
-    ImageId="ami-0e86e20dae9224db8",
+    ImageId=ami,
     MinCount=1,
     MaxCount=1,
     InstanceType="t2.micro",
@@ -24,9 +45,10 @@ nginx_instance = ec2_resource.create_instances(
     TagSpecifications=[
         {
             'ResourceType': 'instance',
-            'Tags': [{'Key': 'Name', 'Value': 'MyEC2Instance'},]
+            'Tags': [{'Key': 'Name', 'Value': 'RunSabba Nginx Server'},
+                     {'Key': 'Owner', 'Value': instance_owner }]
         },
     ]
 )
 
-print("The",nginx_instance[0].id," instance has been created.")
+print(f"The {nginx_instance[0].id} instance has been created in the {region_name} region.")
